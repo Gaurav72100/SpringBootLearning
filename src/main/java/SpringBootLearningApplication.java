9cba0d9384aedd4c6c74b5ccf19b0c9dc6c4fce7
Gaurav72100 <gk240662@gmail.com>
2025-02-16 22:42:58 +0530

    Initial commit

---
+++ src/main/java/SpringBootLearningApplication.java
@@ -0,0 +1,19 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class SpringBootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningApplication.class, args);
+		System.out.println("hi.......");
+
+	}
+	
+	
+}
+  
